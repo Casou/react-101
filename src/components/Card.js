962f0c048198c288cc6430d6cd4a11b6df16a7a2
Basile <basile.parent@arvato.fr>
2019-06-18 16:30:21 +0200

    Update Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,31 @@ import './Card.css';
 
 const Card = ({ message }) => (
     <section className="card person-card">
+        <div className="card-image">
+            <img src="https://uinames.com/api/photos/female/1.jpg" />
+        </div>
         <div className="card-content">
-            Insérez le message passé en props ({ message });
+            <div className="card-title">
+                Stéphanie Tixier
+            </div>
+            <div className="card-sub-title">
+                Développeur.se
+            </div>
+            <div className="card-info">
+                <i className="material-icons" title="email">email</i>
+                <span>
+                    <a href="mailto:s.tixier@proxiad.com">
+                        s.tixier@proxiad.com
+                    </a>
+                </span>
+            </div>
+
+            <div className="card-info">
+                <i className="material-icons" title="phone">phone</i>
+                <span>
+                    <a href="tel:0637234221">0637234221</a>
+                </span>
+            </div>
         </div>
     </section>
 );
