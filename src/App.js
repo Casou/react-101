472e6555ac56e304c6ba73112837611d176b3f10
Basile <basile.parent@arvato.fr>
2019-06-18 14:18:15 +0200

    Prepare the exercice

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
 import React from 'react';
 import './App.css';
-import HeaderBar from "./components/app-bar/HeaderBar";
-import {Redirect, Route, Switch} from "react-router-dom";
-import ListPeople from "./pages/list-people/ListPeople";
-import RandomPeople from "./pages/random-people/RandomPeople";
-import EditPerson from "./pages/edit-person/EditPerson";
-import {CircularProgress} from "@material-ui/core";
-import {loadPeople, savePerson} from "./store/people/actions";
-import {withStorePeople} from "./components/hoc/withStorePeople";
-import {getAllPeople, updatePerson} from "./service/people";
 
-// With action creators :
-// import PeopleActions from "./store/people/actions";
-// import {bindActionCreators} from "redux";
+const MESSAGE = "CE MESSAGE";
 
-class App extends React.Component {
+const App = () => (
+  <div className="App">
+    <header>insérez votre AppBar ici</header>
+    <main>insérez votre Card ici et passez "{MESSAGE}" comme prop</main>
+  </div>
+);
 
-  componentDidMount() {
-    this.props.loadPeople();
-
-    // With action creators :
-    // this.props.peopleActions.loadPeople();
-  }
-
-  render() {
-    const { people, savePerson } = this.props;
-
-    return (
-      <div className="App">
-        <header>
-          <HeaderBar />
-        </header>
-        <main>
-          {
-            !people.length ?
-              <CircularProgress />
-            :
-              <Switch>
-                <Route path="/people" component={ ListPeople } exact />
-                <Route path="/random" component={ RandomPeople } exact />
-                <Route path="/people/edit/:id" render={ (props) => <EditPerson {...props} onSave={savePerson} /> } exact />
-                <Redirect to="/people" />
-              </Switch>
-          }
-        </main>
-      </div>
-    );
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-  // With action creators :
-  // peopleActions: bindActionCreators(PeopleActions, dispatch)
-  loadPeople: () => dispatch(loadPeople(getAllPeople)),
-  savePerson: (person) => dispatch(savePerson(() => updatePerson(person)))
-});
-
-export default withStorePeople(App, mapDispatchToProps);
-
+export default App;
