a25e48d24b3f080400d3bbcfc3a69f5ba79f4084
Basile <basile.parent@arvato.fr>
2019-06-18 13:09:18 +0200

    Typo

--- src/pages/list-people/components/person-card/PersonCard.js
+++ src/pages/list-people/components/person-card/PersonCard.js
@@ -11,7 +11,7 @@ const PersonCard = ({ person, onClick }) => (
       <CardActionArea>
         <CardMedia
           classes={{root : "card_media"}}
-          image={ `https://uinames.com/api/photos/${ person.sex === 1 ? "male" : "female" }/${ person.pictureIndex + 1}.jpg` }
+          image={ `https://uinames.com/api/photos/${ person.sex === 1 ? "male" : "female" }/${ person.pictureIndex }.jpg` }
           title={`Picture of ${person.firstName}`}
         />
         <CardContent>
