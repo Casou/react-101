a9504c45b0bddc7cec36d7f9de4982595fd88265
Basile <basile.parent@arvato.fr>
2019-06-18 14:53:06 +0200

    Remove props from step 01

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const MESSAGE = "CE MESSAGE";
 const App = () => (
   <div className="App">
     <header>insérez votre AppBar ici</header>
-    <main>insérez votre Card ici et passez "{MESSAGE}" comme prop</main>
+    <main>insérez votre Card ici</main>
   </div>
 );
 
