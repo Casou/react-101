feede95c21302ade6de9db5f608beabe4890a3a4
Basile <basile.parent@arvato.fr>
2019-06-20 13:26:50 +0200

    Add Button to Random component

--- src/pages/random-people/RandomPeople.jsx
+++ src/pages/random-people/RandomPeople.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PersonCard from "../list-people/components/person-card/PersonCard";
+import Button from '@material-ui/core/Button';
+
 
 import './RandomPeople.css';
 import {withStorePeople} from "../../components/hoc/withStorePeople";
 
-const RandomPeople = ({ people }) => {
-	return (
-		<main id={"random"}>
-			<PersonCard person={ people[Math.floor(Math.random() * people.length)] } />
-		</main>
-	);
+class RandomPeople extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			personIndex : 0
+		};
+	}
+
+	componentDidMount() {
+		this._updatePerson();
+	}
+
+	_updatePerson = () => {
+		const { people } = this.props;
+		this.setState({ personIndex : Math.floor(Math.random() * people.length) });
+	}
+
+	render() {
+		const { people } = this.props;
+		const { personIndex } = this.state;
+
+		return (
+			<main id={"random"}>
+				<PersonCard person={ people[personIndex] } />
+				<Button variant="contained" onClick={this._updatePerson}>Next</Button>
+			</main>
+		);
+	}
+	
 };
 
 RandomPeople.propTypes = {
