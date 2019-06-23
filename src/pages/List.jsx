e8369a8062379c63a85b55b96180cfb239592fbb
Casou <casou.acc@gmail.com>
2019-06-23 16:41:52 +0200

    Prepare Step 09

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -11,56 +11,21 @@ export const sortPeople = (a, b) => {
   return a.lastName.localeCompare(b.lastName);
 };
 
-class List extends React.Component {
+const List = ({people}) => {
+  const sortedPeople = people.sort(sortPeople);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchText: ""
-    }
-  }
-
-  _searchChanged = (e) => {
-    this.setState({ searchText : e.target.value });
-  };
-
-  _filterPeople = () => {
-    const { people } = this.props;
-    const { searchText } = this.state;
-
-    let filteredPeople = people;
-    if (searchText) {
-      filteredPeople = filteredPeople.filter(p =>
-        p.lastName.indexOf(searchText) >= 0 || p.firstName.indexOf(searchText) >= 0);
-    }
-
-    return filteredPeople.sort(sortPeople);
-  };
-
-  render() {
-    const { people } = this.props;
-    const { searchText } = this.state;
-    const sortedPeople = this._filterPeople(people);
-
-    return (
-      <Fragment>
-        <SearchInput placeholder={"Chercher par nom"}
-                     value={searchText}
-                     onChange={this._searchChanged}
-        />
-
-        <article id={"list-people"}>
-          {
-            sortedPeople.map(p =>
-              <PersonCard key={p.id}
-                          person={p}
-              />)
-          }
-        </article>
-      </Fragment>
-    );
-  }
+  return (
+    <Fragment>
+      <article id={"list-people"}>
+        {
+          sortedPeople.map(p =>
+            <PersonCard key={p.id}
+                        person={p}
+            />)
+        }
+      </article>
+    </Fragment>
+  );
 }
 
 export default List;
