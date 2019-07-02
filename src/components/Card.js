f3674821b35713d4ee318d4001a3b46432903c66
Basile <basile.parent@arvato.fr>
2019-07-02 14:53:22 +0200

    Delete unnecessary files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-const Card = (props) => (
+const Card = () => (
     <section className="card person-card">
         <div className="card-content">
             Insérez le message passé en props (ici);
