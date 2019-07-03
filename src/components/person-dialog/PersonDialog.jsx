dfa25ef174040cae0a5577d25a54933e98b0122c
Casou <casou.acc@gmail.com>
2019-07-03 22:35:19 +0200

    WIP

---
+++ src/components/person-dialog/PersonDialog.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Dialog, DialogContent, Icon, Link} from "@material-ui/core";
+import {lpad} from "common";
+
+import "./PersonDialog.css";
+
+const PersonDialog = ({ open, handleClose, person }) => {
+  if (!person) {
+    return "";
+  }
+
+  const photo = `https://uinames.com/api/photos/${ person.sex === 1 ? "male" : "female" }/${ person.pictureIndex }.jpg`;
+
+  const birthdate = lpad(person.birthDate[2]) + "/" + lpad(person.birthDate[1]) + "/" + lpad(person.birthDate[0]);
+  const arrivalDate = lpad(person.arrivalDate[2]) + "/" + lpad(person.arrivalDate[1]) + "/" + lpad(person.arrivalDate[0]);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      scroll={'paper'}
+      aria-labelledby="scroll-dialog-title"
+      classes={{root : "person-dialog"}}
+      fullWidth={true}
+      maxWidth={'md'}
+    >
+      <DialogContent classes={{root : "person-dialog__content"}}>
+        <div id="person-dialog__background"><div id="person-dialog__background-overlay"></div></div>
+        <div id={"person-dialog__head"}>
+          <div id={"person-dialog__head-img"}>
+            <img src={ photo } alt={ photo } />
+            <h1>{person.firstName} {person.lastName}</h1>
+            <h2>
+              {person.job}
+              <Link href={`/people/edit/${person.id}`}>
+                <Icon className={"edit_icon"}>edit</Icon>
+              </Link>
+            </h2>
+          </div>
+
+          <aside>
+            <h1>Profil :</h1>
+            <ul>
+              <li><label>Date de naissance : </label><span>{ birthdate } ({ person.age } ans)</span></li>
+              <li><label>Date d'arrivée : </label><span>{ arrivalDate }</span></li>
+              <li><label>Email : </label><span>{ person.email }</span></li>
+              <li><label>Téléphone : </label><span>{ person.phone }</span></li>
+              <li><label>Lieu de travail : </label><span>{ person.workPlace }</span></li>
+            </ul>
+          </aside>
+        </div>
+
+        <main>
+          <div>
+            <h1>
+              <div className="person-dialog__icon">
+                <Icon>computer</Icon>
+              </div>
+              Compétences
+            </h1>
+            {
+              Object.entries(person.skillMap).map(([key, value]) =>
+                <div key={ key } className="person-dialog__skill-category">
+                  <h2>{ key }</h2>
+                  <ul>
+                    { value.map(skill => <li key={skill}>{ skill }</li>) }
+                  </ul>
+                </div>
+              )
+            }
+          </div>
+
+          <div>
+            <h1>
+              <div className="person-dialog__icon">
+                <Icon>comment</Icon>
+              </div>
+              Langues
+            </h1>
+            <div className="person-dialog__languages">
+              <ul>
+                {
+                  person.languages.map(language =>
+                    <li key={ language.icon }>
+                      <div className="person-dialog__languages-icon">
+                        <i className={"flagIcon " + language.icon} title={language.icon} />
+                      </div>
+                      <label>{ language.label }</label>
+                    </li>
+                  )
+                }
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <h1>
+              <div className="person-dialog__icon">
+                <Icon>directions_bike</Icon>
+              </div>
+              Intérêts
+            </h1>
+            <div className="person-dialog__interets">
+              <ul>
+                {
+                  person.interets.map(interet =>
+                    <li key={ interet }>
+                      <Icon className="icon">check</Icon>
+                      { interet }
+                    </li>
+                  )
+                }
+              </ul>
+            </div>
+          </div>
+        </main>
+
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+PersonDialog.propTypes = {
+  open : PropTypes.bool.isRequired,
+  handleClose : PropTypes.func.isRequired,
+  person : PropTypes.any
+};
+
+
+export default PersonDialog;
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,6 +3,7 @@ import PersonCard from "../components/person-card/PersonCard";
 
 import './List.css';
 import SearchInput from "../components/search-input/SearchInput";
+import PersonDialog from "../components/person-dialog/PersonDialog";
 
 export const sortPeople = (a, b) => {
   if (a.lastName === b.lastName) {
@@ -13,13 +14,11 @@ export const sortPeople = (a, b) => {
 
 class List extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchText: ""
-    }
-  }
+  state = {
+    searchText: "",
+    dialogOpen: false,
+    selectedPerson : null
+  };
 
   _searchChanged = (e) => {
     this.setState({ searchText : e.target.value });
@@ -39,9 +38,12 @@ class List extends React.Component {
     return filteredPeople.sort(sortPeople);
   };
 
+  _openDialog = (selectedPerson) => this.setState({ dialogOpen : true, selectedPerson });
+  _closeDialog = () => this.setState({ dialogOpen : false });
+
   render() {
     const { people } = this.props;
-    const { searchText } = this.state;
+    const { searchText, dialogOpen, selectedPerson } = this.state;
     const sortedPeople = this._filterPeople(people);
 
     return (
@@ -56,9 +58,15 @@ class List extends React.Component {
             sortedPeople.map(p =>
               <PersonCard key={p.id}
                           person={p}
+                          onClick={() => this._openDialog(p)}
               />)
           }
         </article>
+
+        <PersonDialog open={dialogOpen}
+                      handleClose={this._closeDialog}
+                      person={selectedPerson}
+        />
       </Fragment>
     );
   }
