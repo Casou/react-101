fcaf8175e168285091ec9d844b69546768b0122f
Casou <casou.acc@gmail.com>
2020-10-11 10:27:17 +0200

    Refacto for Atecna

--- src/components/person-card/PersonCard.js
+++ src/components/person-card/PersonCard.js
@@ -9,7 +9,7 @@ const PersonCard = ({ person }) => (
       <CardActionArea>
         <CardMedia
           classes={{root : "card_media"}}
-          image={ `https://uinames.com/api/photos/${ person.sex === 1 ? "male" : "female" }/${ person.pictureIndex }.jpg` }
+          image={ `https://randomuser.me/api/portraits/${ person.sex === 1 ? "male" : "female" }/${ person.pictureIndex }.jpg` }
           title={`Picture of ${person.firstName}`}
         />
         <CardContent>
