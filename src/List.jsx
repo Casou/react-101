cceaff2d99593cc1089786a7d6eb14130825f608
Basile <casou.acc@gmail.com>
2020-10-15 14:53:10 +0200

    Better solution for sort

--- src/List.jsx
+++ src/List.jsx
@@ -3,27 +3,21 @@ import PersonCard from "./components/person-card/PersonCard";
 
 import './List.css';
 
-export const sortPeople = (a, b) => {
-	if (a.lastName === b.lastName) {
-		return a.firstName.localeCompare(b.firstName);
-	}
-	return a.lastName.localeCompare(b.lastName);
-};
-
-const List = ({ people }) => {
-	const sortedPeople = people.sort(sortPeople);
-	return (
-		<Fragment>
-			<article id={"list-people"}>
-				{
-					sortedPeople.map(p =>
-						<PersonCard key={ p.id }
-									person={p}
-						/>)
-				}
-			</article>
-		</Fragment>
-	)
+const List = ({people}) => {
+  return (
+    <Fragment>
+      <article id={"list-people"}>
+        {
+          people
+						.sort((a, b) => a.lastName.localeCompare(b.lastName) || a.firstName.localeCompare(b.firstName))
+            .map(p =>
+              <PersonCard key={p.id}
+                          person={p}
+              />)
+        }
+      </article>
+    </Fragment>
+  )
 };
 
 export default List;
--- src/common.js
+++ src/common.js
@@ -2,10 +2,3 @@ export const lpad = (number, width = 2, character = '0') => {
 	number = number + '';
 	return number.length >= width ? number : new Array(width - number.length + 1).join(character) + number;
 };
-
-export const sortPeople = (a, b) => {
-	if (a.lastName === b.lastName) {
-		return a.firstName.localeCompare(b.firstName);
-	}
-	return a.lastName.localeCompare(b.lastName);
-};
