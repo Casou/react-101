bf130b863b1d93165eebdb428e3b8f85ef755f49
Basile Parent <casou.acc@gmail.com>
2022-02-10 23:58:28 +0100

    Solution for Clock exerciseÃ

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,13 +3,31 @@ import PropTypes from 'prop-types';
 
 import "./Clock.css"
 
-const Clock = (props) => {
-  return (
-    <span className="clock__value">
-      12:34:56
-    </span>
-  )
-};
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTime: new Date()
+    }
+
+    setInterval(() => {
+      this.setState({
+        currentTime: new Date()
+      })
+    }, 1000)
+  }
+
+  render() {
+    const { currentTime } = this.state;
+
+    return (
+      <span className="clock__value">
+        { currentTime.toLocaleTimeString() }
+      </span>
+    )
+  }
+}
 
 Clock.propTypes = {};
 Clock.defaultProps = {};
