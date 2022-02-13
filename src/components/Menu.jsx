9d3a109545e1b065672d25e644feba42f1a7bfd8
Basile Parent <basile.parent.pro@gmail.com>
2022-02-13 11:44:20 +0100

    Initialize list exercise

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,38 @@
 import PropTypes from 'prop-types';
 import "./Menu.css";
-import DishCard from "./DishCard";
+import Dish from "./Dish";
 
 const Menu = ({recipes}) => {
     return (
         <div className="menu">
-            <DishCard recipe={recipes[0]}/>
-            <DishCard recipe={recipes[1]}/>
-            <DishCard recipe={recipes[2]}/>
-            <DishCard recipe={recipes[3]}/>
+            <Dish picture={<Dish.Picture dish={recipes[0]}/>}>
+                <Dish.Title dish={recipes[0]}/>
+                <Dish.Category dish={recipes[0]}/>
+                <Dish.VideoLink dish={recipes[0]}/>
+                <Dish.ThumbnailLink dish={recipes[0]}/>
+                <Dish.Tags dish={recipes[0]}/>
+            </Dish>
+            <Dish picture={<Dish.Picture dish={recipes[1]}/>}>
+                <Dish.Title dish={recipes[1]}/>
+                <Dish.Category dish={recipes[1]}/>
+                <Dish.VideoLink dish={recipes[1]}/>
+                <Dish.ThumbnailLink dish={recipes[1]}/>
+                <Dish.Tags dish={recipes[1]}/>
+            </Dish>
+            <Dish picture={<Dish.Picture dish={recipes[2]}/>}>
+                <Dish.Title dish={recipes[2]}/>
+                <Dish.Category dish={recipes[2]}/>
+                <Dish.VideoLink dish={recipes[2]}/>
+                <Dish.ThumbnailLink dish={recipes[2]}/>
+                <Dish.Tags dish={recipes[2]}/>
+            </Dish>
+            <Dish picture={<Dish.Picture dish={recipes[3]}/>}>
+                <Dish.Title dish={recipes[3]}/>
+                <Dish.Category dish={recipes[3]}/>
+                <Dish.VideoLink dish={recipes[3]}/>
+                <Dish.ThumbnailLink dish={recipes[3]}/>
+                <Dish.Tags dish={recipes[3]}/>
+            </Dish>
         </div>
     )
 }
