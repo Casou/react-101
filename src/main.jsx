1ed123c5a6e873ce14878310d0589bd8bbf0d496
Basile Parent <casou.acc@gmail.com>
2022-02-17 00:48:21 +0100

    Cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Menu from "./components/Menu";
-import RandomDish from "./components/RandomDish";
-import SingleDish from "./components/SingleDish";
 
 ReactDOM.render(
     <React.StrictMode>
