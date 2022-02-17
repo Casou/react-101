dc1458ac67443350b692f08b1f653083dbdbc8cd
Basile Parent <casou.acc@gmail.com>
2022-02-17 01:24:09 +0100

    Solution to fetch exercise

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,37 @@ import {Navigate, Routes, Route} from "react-router-dom";
 import Menu from "./components/Menu";
 import SingleDish from "./components/SingleDish";
 import RandomDish from "./components/RandomDish";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Loader from "./components/Loader";
 
 function App() {
+  const [ recipes, setRecipes ] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/recipes")
+      .then(r => r.json())
+      .then(setRecipes);
+  }, []);
+
   return (
     <div className="App">
       <header>
         <AppBar />
       </header>
       <main>
-        <Routes>
-          <Route path={"/menu"}>
-            <Route index element={<Menu recipes={[]} />} />
-            <Route path={":id"} element={<SingleDish recipes={[]} />} />
-          </Route>
-          <Route path={"/random"} element={<RandomDish recipes={[]} />} />
+        {
+          !recipes ?
+            <Loader /> :
+            <Routes>
+              <Route path={"/menu"}>
+                <Route index element={<Menu recipes={recipes} />} />
+                <Route path={":id"} element={<SingleDish recipes={recipes} />} />
+              </Route>
+              <Route path={"/random"} element={<RandomDish recipes={recipes} />} />
 
-          <Route path="*" element={<Navigate to="/menu" replace />} />
-        </Routes>
+              <Route path="*" element={<Navigate to="/menu" replace />} />
+            </Routes>
+        }
       </main>
     </div>
   )
