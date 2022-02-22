3cb80562707185fa02accd6257cf3745c863b34e
Basile Parent <casou.acc@gmail.com>
2022-02-22 20:06:02 +0100

    Cleanup

--- src/common/hoc/withLoading.jsx
+++ src/common/hoc/withLoading.jsx
@@ -1,4 +1,3 @@
-import {connect} from "react-redux";
 import Loader from "../components/Loader";
 import React from "react";
 
@@ -10,22 +9,5 @@ const withLoading = (WrappedComponent) => ({ recipes, ...props }) => {
   }
   return <WrappedComponent {...props} recipes={recipes} />
 };
-
-// // More complex solution: the wrapped component is not connected to the store
-// // so we need to connect the HOC
-// const mapStateToProps = (store) => ({
-//   r: store.recipes,
-// });
-//
-// const withLoading = (WrappedComponent) => {
-//   const Loading = ({r, ...props}) => {
-//     if (!r) {
-//       return <Loader/>;
-//     }
-//     return <WrappedComponent {...props} recipes={r} />
-//   };
-//
-//   return connect(mapStateToProps)(Loading);
-// };
 
 export default withLoading;
