be1243657e4b78e43740d3ed1addfd884df26c79
Basile Parent <casou.acc@gmail.com>
2022-02-22 19:01:22 +0100

    Fix edit link

--- src/pages/Menu/components/RecipeDialog/EditIcon.jsx
+++ src/pages/Menu/components/RecipeDialog/EditIcon.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from "./RecipeDialog.module.css";
+import {Link} from "react-router-dom";
 
 const EditIcon = ({ recipeId }) => {
   return (
-    <a href={`/menu/edit/${ recipeId}`} className={styles["modal-edit-icon"]}>
+    <Link to={`/menu/edit/${ recipeId}`} className={styles["modal-edit-icon"]}>
       <i className={"material-icons "}>edit</i>
-    </a>
+    </Link>
   )
 };
 
