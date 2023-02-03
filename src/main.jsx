f2402778b56b83bb00a3115ad5cdd38f98a9f83a
Basile Parent <casou.acc@gmail.com>
2023-02-03 16:03:50 +0100

    Update dependencies to match main

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Menu from "./components/Menu"
+import RandomDish from "./components/RandomDish"
+import SingleDish from "./components/SingleDish"
 import App from './App'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Menu from "./components/Menu";
-import RandomDish from "./components/RandomDish";
-import SingleDish from "./components/SingleDish";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<App/>}>
-          <Route path={"/menu"}>
-            <Route index element={<Menu />} />
-            <Route path={":id"} element={<SingleDish />} />
-          </Route>
-          <Route path={"/random"} element={<RandomDish />} />
+import {createRoot} from "react-dom/client"
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<App/>}>
+        <Route path={"/menu"}>
+          <Route index element={<Menu />} />
+          <Route path={":id"} element={<SingleDish />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+        <Route path={"/random"} element={<RandomDish />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
