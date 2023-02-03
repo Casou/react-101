ae820267e5ccaaa1a7d63bdac5ea978ac9513f32
Basile Parent <casou.acc@gmail.com>
2023-02-03 16:03:50 +0100

    Update dependencies to match main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import App from './App'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/*"} element={<App/>} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+import {createRoot} from "react-dom/client"
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/*"} element={<App/>} />
+    </Routes>
+  </BrowserRouter>
 )
