1adda4cd24afeef23b68db786f6ab24a3fa9638f
Basile Parent <casou.acc@gmail.com>
2023-02-05 19:52:02 +0100

    Write tests

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Dish from "./Dish";
 import DishType from "./DishPropTypes";
+import caloryUtils from "../utils/caloryUtils";
 
 const DishCard = ({ recipe }) => {
     return (
         <Dish picture={<Dish.Picture src={recipe.thumbnail}/>}>
             <Dish.Title>{ recipe.name }</Dish.Title>
             <Dish.Category>{ recipe.category }</Dish.Category>
-            <Dish.VideoLink link={recipe.video} />
-            <Dish.ThumbnailLink link={recipe.thumbnail} />
+            <Dish.VideoLink link={recipe.video}/>
+            <Dish.ThumbnailLink link={recipe.thumbnail}/>
+            <Dish.Calories caloryCount={ caloryUtils.calculateCalories(recipe) } />
             { !!recipe.tags.length && <Dish.Tags tags={recipe.tags}/> }
         </Dish>
     );
