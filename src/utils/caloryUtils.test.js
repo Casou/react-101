15ffa259688acbd4335b5e22cd12116f14000aea
Basile Parent <casou.acc@gmail.com>
2023-02-06 09:00:50 +0100

    Init exercise

--- src/utils/caloryUtils.test.js
+++
@@ -1,9 +0,0 @@
-import caloryUtils from "./caloryUtils";
-
-describe("caloryUtils tests", () => {
-  describe("caloryUtils - calculateCalories tests", () => {
-    it("should return the calculated calory count when the recipe is passed", () => {
-      expect(caloryUtils.calculateCalories({ instructions: "test" })).toEqual(10)
-    })
-  })
-})
