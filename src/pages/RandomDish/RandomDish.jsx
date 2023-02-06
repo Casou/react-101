3e639a657eeb78a779c367341bbe440ffffd0dda
Basile Parent <casou.acc@gmail.com>
2023-02-06 16:27:28 +0100

    Fix init exercise

--- src/pages/RandomDish/RandomDish.jsx
+++ src/pages/RandomDish/RandomDish.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import DishType from "../../types/DishType";
 import DishCard from "../../common/components/DishCard";
+import styles from "./RandomDish.module.css"
 
 const RandomDish = ({ recipes }) => {
     const recipe = recipes[Math.round(Math.random() * recipes.length)];
     return (
-        <div>
+        <div className={styles.content}>
             <DishCard recipe={recipe} />
         </div>
     )
