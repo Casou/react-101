a86e5c8114676a00fdba50c1010e17afbdaebc04
Basile Parent <casou.acc@gmail.com>
2023-02-08 11:34:32 +0100

    Add tests

--- src/pages/Menu/Menu.test.jsx
+++ src/pages/Menu/Menu.test.jsx
@@ -4,6 +4,24 @@ import userEvent from "@testing-library/user-event";
 
 describe("Menu tests", () => {
 
+  it("should display one card by recipe", () => {
+      // Given
+      const recipes = [
+        { id: 1, name: "My Recipe", instructions: "N/A", tags: [] },
+        { id: 2, name: "My second recipe", instructions: "N/A", tags: [] },
+      ]
+
+      // When
+      const {getAllByRole} = render(
+        <Menu recipes={recipes} />
+      )
+
+      // Then
+      const allThumbnail = getAllByRole("img")
+      expect(allThumbnail).toHaveLength(2)
+    }
+  )
+
   it("should display (apply css class) RecipeDialog when clicking on a card", async () => {
 
   })
