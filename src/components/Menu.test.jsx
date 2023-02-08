7b9c5364dea40d89493f4cc318ef1caa463212b0
Basile Parent <casou.acc@gmail.com>
2023-02-08 10:54:36 +0100

    Reinit tests

--- src/components/Menu.test.jsx
+++ src/components/Menu.test.jsx
@@ -3,11 +3,6 @@ import Menu from "./Menu"
 
 describe("Menu tests", () => {
   it("should display 84 cards", () => {
-      const {getAllByRole} = render(
-        <Menu />
-      )
-      const allThumbnail = getAllByRole("img")
-      expect(allThumbnail).toHaveLength(84)
-    }
-  )
+
+  })
 })
--- src/components/SingleDish.test.jsx
+++ src/components/SingleDish.test.jsx
@@ -1,32 +1,8 @@
 import {render} from "@testing-library/react";
-import {createMemoryRouter, RouterProvider} from "react-router-dom";
 import SingleDish from "./SingleDish"
 
 describe("SingleDish tests", () => {
-
-  const createRouter = (recipeId) => {
-    const routes = [
-      {
-        path: "/menu/:id",
-        element: <SingleDish/>,
-      },
-    ];
-    return createMemoryRouter(routes, {
-      initialEntries: [`/menu/${recipeId}`],
-      initialIndex: 0,
-    });
-  }
-
   it("should display the asked recipe", () => {
-    // Given
-    const router = createRouter(52976)
 
-    // When
-    const {getByText} = render(
-      <RouterProvider router={router}/>
-    )
-
-    // Then
-    expect(getByText(/Cashew Ghoriba Biscuits/i)).toBeInTheDocument()
   })
 })
