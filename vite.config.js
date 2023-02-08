d27304ad81d85cdd64fa43d9c732f96d7ee93a4d
Basile Parent <casou.acc@gmail.com>
2023-02-08 12:47:17 +0100

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped',
+      },
+    },
     setupFiles: "./src/__tests__/setup.js",
   }
 })
